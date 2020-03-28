324525523455aff824814c7e4f93f1064c96a1af
hzy <2196260918@qq.com>
2020-03-28 19:16:48 +0800

    拓展测试控制器

--- classes/View/Main/测试/WBdemoViewController.swift
+++ classes/View/Main/测试/WBdemoViewController.swift
@@ -9,22 +9,22 @@
 import UIKit
 
 class WBdemoViewController: wbbaseViewController {
-
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        title = "第\(navigationController?.children.count ?? 0)个"
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+//监听方法
+    @objc private func shownext(){
+        let vc = WBdemoViewController()
+        navigationController?.pushViewController(vc, animated: true)
+        
     }
-    */
-
+}
+extension WBdemoViewController{
+    //重写
+    override func setupui() {
+//        设置右侧控制器
+        super .setupui()
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title:"下一个", style: .plain, target: self, action: #selector(shownext))
+    }
 }
