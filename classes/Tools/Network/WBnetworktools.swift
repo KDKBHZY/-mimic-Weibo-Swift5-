4c5fca6f998013693bf25697fcc110df57b13c2e
hzy <2196260918@qq.com>
2020-04-08 16:25:40 +0800

    创建网络单例

---
+++ classes/Tools/Network/WBnetworktools.swift
@@ -0,0 +1,15 @@
+//
+//  WBnetworktools.swift
+//  传智微博
+//
+//  Created by ZY H on 2020/4/8.
+//  Copyright © 2020 ZY H. All rights reserved.
+//
+
+import UIKit
+import AFNetworking
+//网络管理工具
+class WBnetworktools: AFHTTPSessionManager {
+    //静态区
+static let shared = WBnetworktools()
+}
